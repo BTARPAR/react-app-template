63aad1d4f849a6bdebe77726fe43746ab8a5e08f
Bhargav Tarpara <tarpara_bhargav@yahoo.in>
2023-10-02 14:14:05 -0700

    REPO SET UP

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const {WebpackManifestPlugin} = require('webpack-manifest-plugin');
+
+module.exports = {
+  entry: {
+    bundle: './src/index.js'
+  },
+  output: {
+    filename: '[fullhash].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      // Rule for processing JS or JSX files
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      // Rule for processing CSS files
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      // Rule for processing SCSS files
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename:'./index.html'
+    }),
+    new CleanWebpackPlugin(),
+    new WebpackManifestPlugin(),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 3000,
+    historyApiFallback: true,
+  },
+};
